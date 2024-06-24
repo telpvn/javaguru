bb5f6a1bc1660f17620b2669e778276298410ebd
telpvn <telpvn@mail.ru>
2024-06-24 13:43:29 +0400

    Cycles 1

---
+++ javaguru/Java Core/src/cycles/ArrayTest.java
@@ -0,0 +1,15 @@
+package cycles;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        cycleTest();
+    }
+
+    static void cycleTest() {
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
