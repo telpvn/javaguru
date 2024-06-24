6ae9113b766af6a82f9f10953f4da718314d0be6
telpvn <telpvn@mail.ru>
2024-06-24 16:05:09 +0400

    Arrays 2

--- javaguru/Java Core/src/arrays/ArrayTest.java
+++ javaguru/Java Core/src/arrays/ArrayTest.java
@@ -1,13 +1,15 @@
 package arrays;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class ArrayTest {
 
     public static void main(String[] args) {
         int[] array = new int[10];
+        Random random = new Random();
         for (int i = 0; i < array.length; i++) {
-            array[i] = 10;
+            array[i] = random.nextInt();
         }
 
         System.out.println(Arrays.toString(array));
