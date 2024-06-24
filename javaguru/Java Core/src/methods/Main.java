3a7a22326d768981305a1a19a048d9f66219998e
telpvn <telpvn@mail.ru>
2024-06-24 11:19:17 +0400

    Methods 3

--- javaguru/Java Core/src/methods/Main.java
+++ javaguru/Java Core/src/methods/Main.java
@@ -3,11 +3,16 @@ package methods;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isEven(348));
+        int i = 348;
+        print("Число: " + i + " четное " + isEven(i));
     }
 
     static boolean isEven(int i) {
         return (i % 2) == 0;
+    }
+
+    static void print(String str) {
+        System.out.println(str);
     }
 
 }
