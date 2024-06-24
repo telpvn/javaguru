8458bd64a354f10e8a630a45aa3dd398b1ca1ee9
telpvn <telpvn@mail.ru>
2024-06-24 10:59:01 +0400

    Methods 1

---
+++ javaguru/Java Core/src/methods/Main.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int j = 3;
+        boolean result = isEven(j);
+        System.out.println(result);
+    }
+
+    static boolean isEven(int i) {
+        boolean isEven = (i % 2) == 0;
+        return isEven;
+    }
+
+}
