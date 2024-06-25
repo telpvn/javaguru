a8c79ebe8dc3a5788dcca8da18c472d61d1b055d
telpvn <telpvn@mail.ru>
2024-06-25 16:09:01 +0400

    Classes and objects 1

--- javaguru/Cars/src/Car.java
+++ javaguru/Cars/src/Car.java
@@ -1,5 +1,3 @@
-package сlasses_and_objects;
-
 public class Car {
     String model;
     String color;
@@ -7,25 +5,13 @@ public class Car {
     int volume;
     int fuelLevel;
 
-
     void move(int x1, int y1, int x2, int y2) {
         double path = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         fuelLevel = (int) (fuelLevel - path / 100 * fuelConsumtion);
         if (fuelLevel < 0) {
-            System.out.println("Автомобиль марки: " + model + " не доехал, пора заправляться!");
-            fuelLevel = 0;
+            System.out.println("Автомобиль " + model + " не доехал, пора заправиться!");
         } else {
-            System.out.println("Автомобиль марки: " + model + " прошел: 0" + path + " км, осталось топлива: " + fuelLevel + " литра");
-        }
-    }
-
-    void refuel(int liters) {
-        fuelLevel = fuelLevel + liters;
-        if (fuelLevel > 0) {
-            System.out.println("Бензин льется через край!");
-            fuelLevel = volume;
-        } else {
-            System.out.println("Заправились, теперь у нас " + fuelLevel + " литров");
+            System.out.println("Автомобиль " + model + " прошел: 0" + path + " км. осталось топлива: " + fuelLevel + " литра");
         }
     }
 
@@ -47,8 +33,5 @@ public class Car {
         bmw.fuelConsumtion = 12;
 
         bmw.move(0, 0, 500, 300);
-        bmw.refuel(50);
-        bmw.move(150, 150, 500, 300);
     }
 }
-
