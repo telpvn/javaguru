b46d8cfb21ea0c048aff0b2ad6d63705b1f68de6
telpvn <telpvn@mail.ru>
2024-06-25 13:59:35 +0400

    Classes and objects 2

--- javaguru/Java Core/src/сlasses_and_objects/Car.java
+++ javaguru/Java Core/src/сlasses_and_objects/Car.java
@@ -7,8 +7,54 @@ public class Car {
     int volume;
     int fuelLevel;
 
+    Car() {
+        color = "Белый";
+        fuelConsumtion = 8;
+        volume = 45;
+        fuelLevel = 10;
+    }
+
+    Car(String model) {
+        this.model = model;
+        color = "Белый";
+        fuelConsumtion = 8;
+        volume = 45;
+        fuelLevel = 10;
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = "Белый";
+        fuelConsumtion = 18;
+    }
+
+    public Car(String model, String color, double fuelConsumtion, int volume, int fuelLevel) {
+        this.model = model;
+        this.color = color;
+        this.fuelConsumtion = fuelConsumtion;
+        this.volume = volume;
+        this.fuelLevel = fuelLevel;
+    }
+
     void move(int x1, int y1, int x2, int y2) {
         double path = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println(path);
+        fuelLevel = (int) (fuelLevel - path / 100 * fuelConsumtion);
+        if (fuelLevel < 0) {
+            System.out.println("Автомобиль марки: " + model + " не доехал, пора заправляться!");
+            fuelLevel = 0;
+        } else {
+            System.out.println("Автомобиль марки: " + model + " прошел: 0" + path + " км, осталось топлива: " + fuelLevel + " литра");
+        }
+
+    }
+
+    void refuel(int liters) {
+        fuelLevel = fuelLevel + liters;
+        if (fuelLevel > 0) {
+            System.out.println("Бензин льется через край!");
+            fuelLevel = volume;
+        } else {
+            System.out.println("Заправились, теперь у нас " + fuelLevel + " литров");
+        }
     }
 }
--- javaguru/Java Core/src/сlasses_and_objects/Main.java
+++ javaguru/Java Core/src/сlasses_and_objects/Main.java
@@ -3,10 +3,16 @@ package сlasses_and_objects;
 public class Main {
 
     public static void main(String[] args) {
-        Car opel = new Car();
-        opel.model = "Opel";
+        Car opel = new Car("Opel");
         opel.color = "Желтый";
-
         opel.move(10, 10, 100, 100);
+
+        Car bmw = new Car("BMW", "Черный");
+        bmw.volume = 60;
+
+        bmw.move(0, 0, 500, 300);
+        bmw.refuel(50);
+        bmw.move(150, 150, 500, 300);
+
     }
 }
